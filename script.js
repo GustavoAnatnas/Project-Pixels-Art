28c85e610d1ed8507d97ba52bc008522a3d89886
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-02-15 15:50:23 -0300

    Resolve desafio 2

---
+++ script.js
@@ -0,0 +1,5 @@
+    let color = document.getElementsByClassName("color")
+    color[0].style.background = 'pink';
+    color[1].style.background = 'green';
+    color[2].style.background = 'blue';
+    color[3].style.background = 'yellow';
