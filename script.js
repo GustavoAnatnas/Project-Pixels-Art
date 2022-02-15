9311d57208a027373e2fc791e2f16607db3aa777
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-02-15 19:21:31 -0300

    save

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-    let color = document.getElementsByClassName("color")
+
+let color = document.getElementsByClassName("color")
     color[0].style.background = 'black';
     color[1].style.background = 'pink';
     color[2].style.background = 'blue';
     color[3].style.background = 'yellow';
+
+
+function criaDivs(divs){
+const quadroPixel = document.getElementById('pixel-board');
+    for (let index = 0; index < divs; index++) {
+    let divs = document.createElement('div');
+    divs.className = 'pixel'; 
+    quadroPixel.appendChild(divs);
+}
+}
+criaDivs(25);
+
+
