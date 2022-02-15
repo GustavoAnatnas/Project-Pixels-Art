b9372d1b0ad19ae846e9ab9da65a7312c7124ce7
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-02-15 16:01:53 -0300

    resolve 3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
     let color = document.getElementsByClassName("color")
-    color[0].style.background = 'pink';
-    color[1].style.background = 'green';
+    color[0].style.background = 'black';
+    color[1].style.background = 'pink';
     color[2].style.background = 'blue';
     color[3].style.background = 'yellow';
